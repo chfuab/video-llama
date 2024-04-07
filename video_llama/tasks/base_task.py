49ebc837d476e8154e39b8522fba567849b6ce6c
chfuab <chfuab@gmail.com>
2024-04-07 14:25:28 +0800

    removed importing neg_sampler & select_embed in base_task.py

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -15,7 +15,6 @@ from video_llama.common.dist_utils import get_rank, get_world_size, is_main_proc
 from video_llama.common.logger import MetricLogger, SmoothedValue
 from video_llama.common.registry import registry
 from video_llama.datasets.data_utils import prepare_sample
-from video_llama.datasets.data_utils import neg_sampler, select_embed
 from video_llama.common.eval_metrics import metrics_mapping
 
 
