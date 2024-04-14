6525ef2466c8574975df36f3ce631538ff5c6dc4
chfuab <chfuab@gmail.com>
2024-04-14 19:33:17 +0800

    print testing

--- video_llama/models/blip2.py
+++ video_llama/models/blip2.py
@@ -51,6 +51,7 @@ class Blip2Base(BaseModel):
         encoder_config.add_cross_attention = True
         encoder_config.cross_attention_freq = cross_attention_freq
         encoder_config.query_length = num_query_token
+        print("testing")
         Qformer = BertLMHeadModel(config=encoder_config)
         query_tokens = nn.Parameter(
             torch.zeros(1, num_query_token, encoder_config.hidden_size)
