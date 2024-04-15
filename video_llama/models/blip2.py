4974d3bad2212bcfaf735640dbe71665f9ec61d9
chfuab <chfuab@gmail.com>
2024-04-15 22:09:45 +0800

    removed testing

--- video_llama/models/blip2.py
+++ video_llama/models/blip2.py
@@ -52,7 +52,6 @@ class Blip2Base(BaseModel):
         encoder_config.cross_attention_freq = cross_attention_freq
         encoder_config.query_length = num_query_token
         Qformer = BertLMHeadModel(config=encoder_config)
-        logging.info("loaded Q-former!!!")
         query_tokens = nn.Parameter(
             torch.zeros(1, num_query_token, encoder_config.hidden_size)
         )
--- video_llama/models/eva_vit.py
+++ video_llama/models/eva_vit.py
@@ -431,15 +431,13 @@ def create_eva_vit_g(img_size=224,drop_path_rate=0.4,use_checkpoint=False,precis
         url, check_hash=False, progress=True
     )
     state_dict = torch.load(cached_file, map_location="cpu") """    
-    print("before loading model")
+
     state_dict = torch.load("/mnt/disks/chfuab/eva_vit_g.pth", map_location="cuda:0")   
     interpolate_pos_embed(model,state_dict)
-    print("before load_state_dict")
+
     incompatible_keys = model.load_state_dict(state_dict, strict=False)
 #     print(incompatible_keys)
-    print("after loading model")
     if precision == "fp16":
 #         model.to("cuda") 
         convert_weights_to_fp16(model)
-    print("testing")
     return model
