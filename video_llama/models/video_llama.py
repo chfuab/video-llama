069897b633ed0d8e3c6e655eed8630195057eebe
chfuab <chfuab@gmail.com>
2024-04-19 20:38:17 +0800

    load imgbind ckpt with map_location to be cuda:0

--- video_llama/models/video_llama.py
+++ video_llama/models/video_llama.py
@@ -91,7 +91,7 @@ class VideoLLAMA(Blip2Base):
             self.audio_encoder,self.audio_hidden_size = \
                 imagebind_model.imagebind_huge()
             # self.audio_encoder.load_state_dict(torch.load("{}/imagebind_huge.pth".format(imagebind_ckpt_path), map_location="cuda:1"))
-            imagebind_temp = torch.load("{}/imagebind_huge.pth".format(imagebind_ckpt_path), map_location="cpu")
+            imagebind_temp = torch.load("{}/imagebind_huge.pth".format(imagebind_ckpt_path), map_location="cuda:0")
             self.audio_encoder.load_state_dict(imagebind_temp)
             print("end testing")
 
