1f0e65be847f52019418fab08773a83a0f88a1e7
chfuab <chfuab@gmail.com>
2024-04-19 22:07:58 +0800

    a

--- video_llama/models/video_llama.py
+++ video_llama/models/video_llama.py
@@ -87,13 +87,6 @@ class VideoLLAMA(Blip2Base):
     ):
         super().__init__()
 
-        if equip_audio_branch:
-            print (f'Initializing audio encoder from {imagebind_ckpt_path} ...')
-            self.audio_encoder,self.audio_hidden_size = \
-                imagebind_model.imagebind_huge()
-            self.audio_encoder.load_state_dict(torch.load("{}/imagebind_huge.pth".format(imagebind_ckpt_path), map_location=torch.device("cuda:1"), mmap=True))
-            print("fuck you")
-
         self.tokenizer = self.init_tokenizer()
         self.low_resource = low_resource
         print('Loading VIT')
