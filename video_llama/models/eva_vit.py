8dc9023b46d2bce54438e439120e2eab9cd1511e
chfuab <chfuab@gmail.com>
2024-04-22 22:07:55 +0800

    a

--- video_llama/models/eva_vit.py
+++ video_llama/models/eva_vit.py
@@ -432,7 +432,7 @@ def create_eva_vit_g(img_size=224,drop_path_rate=0.4,use_checkpoint=False,precis
     )
     state_dict = torch.load(cached_file, map_location="cpu") """    
 
-    state_dict = torch.load("/mnt/disks/chfuab/eva_vit_g.pth", map_location="cuda:0", mmap=True)   
+    state_dict = torch.load("/mnt/disks/chfuab/eva_vit_g.pth", map_location=torch.device("cuda:0"), mmap=True)   
     interpolate_pos_embed(model,state_dict)
 
     incompatible_keys = model.load_state_dict(state_dict, strict=False)
