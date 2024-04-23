c3b66f7674852358e8b80c597a7942ad1c228989
chfuab <chfuab@gmail.com>
2024-04-23 21:07:01 +0800

    a

--- video_llama/models/eva_vit.py
+++ video_llama/models/eva_vit.py
@@ -431,9 +431,7 @@ def create_eva_vit_g(img_size=224,drop_path_rate=0.4,use_checkpoint=False,precis
         url, check_hash=False, progress=True
     )
     state_dict = torch.load(cached_file, map_location="cpu") """    
-    print("fuck")
     state_dict = torch.load("/mnt/disks/chfuab/eva_vit_g.pth", map_location=torch.device("cuda:0"))
-    print("fuck your mother")
     interpolate_pos_embed(model,state_dict)
 
     incompatible_keys = model.load_state_dict(state_dict, strict=False)
--- video_llama/models/video_llama.py
+++ video_llama/models/video_llama.py
@@ -284,7 +284,7 @@ class VideoLLAMA(Blip2Base):
             print (f'Initializing audio encoder from {imagebind_ckpt_path} ...')
             self.audio_encoder,self.audio_hidden_size = \
                 imagebind_model.imagebind_huge()
-            self.audio_encoder.load_state_dict(torch.load("{}/imagebind_huge.pth".format(imagebind_ckpt_path), map_location=torch.device("cuda:1"), mmap=True))
+            self.audio_encoder.load_state_dict(torch.load("{}/imagebind_huge.pth".format(imagebind_ckpt_path), map_location=torch.device("cuda:2"), mmap=True))
             # free vision encoder
             for name, param in self.audio_encoder.named_parameters():
                 param.requires_grad = False
