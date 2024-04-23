0606bce4348b546577b302770f6ae4dbd7bcac3f
chfuab <chfuab@gmail.com>
2024-04-23 21:16:16 +0800

    remove map location mmap

--- video_llama/models/video_llama.py
+++ video_llama/models/video_llama.py
@@ -88,7 +88,7 @@ class VideoLLAMA(Blip2Base):
         super().__init__()
         self.audio_encoder,self.audio_hidden_size = \
             imagebind_model.imagebind_huge()
-        self.audio_encoder.load_state_dict(torch.load("{}/imagebind_huge.pth".format(imagebind_ckpt_path), map_location=torch.device("cuda:2"), mmap=True))
+        self.audio_encoder.load_state_dict(torch.load("{}/imagebind_huge.pth".format(imagebind_ckpt_path)))
         
 
         
