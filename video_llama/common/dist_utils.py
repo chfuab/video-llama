9a9ab2fc613af6219d1c180f2d5b828b616a6814
chfuab <chfuab@gmail.com>
2024-05-10 21:01:35 +0800

    a

--- video_llama/common/dist_utils.py
+++ video_llama/common/dist_utils.py
@@ -57,9 +57,9 @@ def is_main_process():
 def init_distributed_mode(args):
     if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
         args.rank = int(os.environ["RANK"])
-        args.world_size = int(os.environ["WORLD_SIZE"])
+        # args.world_size = int(os.environ["WORLD_SIZE"])
+        args.world_size = 1
         args.gpu = int(os.environ["LOCAL_RANK"])
-        print("fuck")
     elif "SLURM_PROCID" in os.environ:
         args.rank = int(os.environ["SLURM_PROCID"])
         args.gpu = args.rank % torch.cuda.device_count()
