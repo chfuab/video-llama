13198cb4046558330eb1dade730fe1dfe8113c07
chfuab <chfuab@gmail.com>
2024-05-10 21:42:51 +0800

    a

--- video_llama/common/dist_utils.py
+++ video_llama/common/dist_utils.py
@@ -58,7 +58,7 @@ def init_distributed_mode(args):
     if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
         args.rank = int(os.environ["RANK"])
         # args.world_size = int(os.environ["WORLD_SIZE"])
-        args.world_size = 0
+        args.world_size = 3
         args.gpu = int(os.environ["LOCAL_RANK"])
     elif "SLURM_PROCID" in os.environ:
         args.rank = int(os.environ["SLURM_PROCID"])
