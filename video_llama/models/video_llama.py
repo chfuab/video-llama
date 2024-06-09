06ae08eb5efab59e591f7a734d93984937831f1d
chfuab <chfuab@gmail.com>
2024-06-09 21:07:45 +0800

    a

--- video_llama/models/video_llama.py
+++ video_llama/models/video_llama.py
@@ -485,11 +485,8 @@ class VideoLLAMA(Blip2Base):
                 position_embedding_A = self.audio_position_embedding(position_id_list_A)  
                 position_embedding_A_all_list.append(position_embedding_A)
             # calculate visual position embedding:
-            
-                position_id_img_frms = torch.LongTensor([pair[0].int() for pair in image_frame_idx_all[k]]).to(device_A)
-
-                print(f"\n\n\n position_id_img_frms: {position_id_img_frms} \n\n\n")
-
+                print(f"\n\n\n pos_id_imgs {[pair[0].int() for pair in image_frame_idx_all[k]]} \n\n\n")
+                position_id_img_frms = torch.LongTensor([pair[0].int() for pair in image_frame_idx_all[k]]).to(device_V)
                 position_embed_V = self.audio_visual_position_embedding(position_id_img_frms)
                 position_embedding_V_all_list.append(position_embed_V)
 
