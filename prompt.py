adc3e3ebca7e85520c9f841f2c92a9befb2a43d2
chfuab <chfuab@gmail.com>
2024-06-14 23:46:22 +0800

    a

--- prompt.py
+++ prompt.py
@@ -9,8 +9,7 @@ if __name__ == '__main__':
     tokenizer = LlamaTokenizer.from_pretrained(model_name)
 
     sys_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
-    context = "Alice has brain."
-    user_prompt = "Does Alice have brain? Answer this question based on below information: \n Alice has brain."
+    user_prompt = "Given that Alice is a human, can you tell me whether Alice have brain?"
 
     prompt = f'''<s>[INST] <<SYS>>
     {sys_prompt}
