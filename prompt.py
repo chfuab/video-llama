12957476e7af11a6dd75d26f430e0c39add67a32
chfuab <chfuab@gmail.com>
2024-06-16 17:24:56 +0800

    added vast27m

--- prompt.py
+++ prompt.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     model = LlamaForCausalLM.from_pretrained(model_name).to("cuda:0")
     tokenizer = LlamaTokenizer.from_pretrained(model_name)
 
-    rule = "If the creature has brain then it's species is human."
+    rule = "If the creature has brain, then it's species is human."
     description = "The creature has brain."
 
     sys_prompt = "You are a helpful assistant. You are given a description about a creature and a rule to determine what species the creature is. Your task is to apply the rule to the description and determine what species the creature is. Give an answer about what species the creature is. The answer should be one sentence long. If you don't know or have not enough information about the answer, explain why."
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
     model = model.eval()
     with torch.no_grad():
-        generate_ids = model.generate(encoding.input_ids, max_length=256, temperature=0.25)
+        generate_ids = model.generate(encoding.input_ids, max_length=256, temperature=2)
         result = tokenizer.decode(generate_ids[0], skip_special_tokens=True)
 
     print(result)
