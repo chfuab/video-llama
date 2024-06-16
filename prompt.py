5c5e4cda2b86881b09032040a8db421fbaf2014d
chfuab <chfuab@gmail.com>
2024-06-16 14:31:52 +0800

    added vast27m

--- prompt.py
+++ prompt.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     tokenizer = LlamaTokenizer.from_pretrained(model_name)
 
     # sys_prompt = "You are a helpful assistant. You are given a description about a creature and a rule to determine what species the creature is. Your task is to apply the rule to the description and determine what species the creature is. Give an answer about what species the creature is. The answer should be one sentence long. If you don't know or have not enough information about the answer, explain why."
-    sys_prompt = "You are given a question. Your task is to answer the question. If you don't know the answer, explain why."
+    sys_prompt = "You are given a question. Your task is to answer the question. Your answer should be of one sentence long. If you don't know the answer, explain why."
     
     # user_prompt = "Rule: If the creature has brain then it's species is human. Description: The creature has brain. What species is the creature? Answer:"
     user_prompt = "What is ChatGPT?"
