27331fad250fb48ee087994af878efe555526149
chfuab <chfuab@gmail.com>
2024-06-16 14:06:13 +0800

    added vast27m

--- prompt.py
+++ prompt.py
@@ -8,16 +8,25 @@ if __name__ == '__main__':
     model = LlamaForCausalLM.from_pretrained(model_name).to("cuda:0")
     tokenizer = LlamaTokenizer.from_pretrained(model_name)
 
-    sys_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
-    user_prompt = "Given that Alice is a human, can you tell me whether Alice have brain?"
+    sys_prompt = '''You are a helpful assistant. You are given a description about a creature and a rule to determine what species the creature is. 
+    Your task is to apply the rule to the description and determine what species the creature is.
+    Give an answer about what species the creature is. The answer should be one sentence long. 
+    If you don't know the answer, explain why you don't know the answer.
+    '''
+    
+    
+    
+    user_prompt = '''Description: The creature has brain.
+    Rule: If the creature has brain then it's species is human.
+    Answer:'''
 
     prompt = f'''<s>[INST] <<SYS>>
     {sys_prompt}
     <</SYS>>
     {user_prompt} [/INST] '''
 
-    # encoding = tokenizer(prompt, return_tensors="pt").to("cuda:0")
-    encoding = tokenizer(user_prompt, return_tensors="pt").to("cuda:0")
+    encoding = tokenizer(prompt, return_tensors="pt").to("cuda:0")
+    # encoding = tokenizer(user_prompt, return_tensors="pt").to("cuda:0")
 
     model = model.eval()
     with torch.no_grad():
