e9f08c42f1a889bcfa31df7213731e9a7a3d687e
chfuab <chfuab@gmail.com>
2024-06-19 21:07:37 +0800

    added vast27m

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -182,6 +182,9 @@ class BaseTask:
         if is_dist_avail_and_initialized():
             dist.barrier()
 
+        assert metric_logger.meters is not None, "\n\n\n metric_logger.meters is none \n\n\n"
+        for k, meter in metric_logger.meters.items():
+            print(f"key: {k}\nvalue: {meter}\n")
         return {
             k: "{:.3f}".format(meter.global_avg)
             for k, meter in metric_logger.meters.items()
