18c91a285c9651cb1048c3b1b8536a7bffdcae93
chfuab <chfuab@gmail.com>
2024-09-30 18:58:38 +0800

    a

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -179,9 +179,9 @@ class BaseTask:
             meters_pair = zip(metric_logger.meters.keys(), meter_values)
             for k, v in meters_pair:
                 print(f"\n\n\n k,v: {k}, {v} \n\n\n")
-                val = v.astype(int)
+                val = int(v[0])
                 
-                metric_logger.update(k=val[0])
+                metric_logger.update(k=val)
 
         logging_str = "Averaged stats: \n" + str(metric_logger.global_avg())    # getting avg over all batch size of samples in one epoch
         logging.info(logging_str)
