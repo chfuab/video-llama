1e6ff9a27f00206aac12d971b2858777c27db71e
chfuab <chfuab@gmail.com>
2024-10-06 10:59:30 +0800

    a

--- video_llama/common/logger.py
+++ video_llama/common/logger.py
@@ -94,7 +94,7 @@ class MetricLogger(object):
                 v = v.item()
             assert isinstance(v, (float, int, str, list, dict, tuple))
             self.meters[k].update(v)
-        print(f"\n\n\n  self.meters: {self.meters} \n\n\n")
+        print(f"\n\n\n  self.meters[ROUGE_L].total: {self.meters["ROUGE_L"].total} \n\n\n self.meters[ROUGE_L].count: {self.meters["ROUGE_L"].count} \n\n\n")
 
     def __getattr__(self, attr):
         if attr in self.meters:
