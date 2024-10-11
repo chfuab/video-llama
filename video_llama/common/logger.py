7d0fd1b905cdebe961a141d672a4cd34d4af74f9
chfuab <chfuab@gmail.com>
2024-10-11 17:31:44 +0800

    a

--- video_llama/common/logger.py
+++ video_llama/common/logger.py
@@ -119,7 +119,8 @@ class MetricLogger(object):
     def global_avg(self):
         loss_str = []
         for name, meter in self.meters.items():
-            loss_str.append("{}: {:.4f}".format(name, meter.total / meter.count))
+            print(f"\n\n\n {meter.total}, {meter.count}  \n\n\n")
+            loss_str.append("{}: {:.4f}".format(name, meter.global_avg()))
         return self.delimiter.join(loss_str)
 
     def synchronize_between_processes(self):
