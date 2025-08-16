72cad0136df8b0b07a0dcfe75fb8e30502569d7d
Chi Ho Fu <chfuab@gmail.com>
2025-08-16 16:49:08 +0800

    a

--- video_llama/processors/transforms_video.py
+++ video_llama/processors/transforms_video.py
@@ -80,10 +80,9 @@ class ResizePadVideo:
         clip_size = torch.tensor([clip_height, clip_width])
 
         dim_pad = torch.argmin(clip_size).item()
-        pad_amount = torch.abs(torch.tensor(clip_height - clip_width)).item()
-        print(f"\n\n {pad_amount}")        
+        pad_amount = torch.abs(torch.tensor(clip_height - clip_width)).item()    
         pad_split = torch.randint(0, pad_amount-1, (1,)).item()
-
+        print(f"\n\n {pad_split}")    
 
         if dim_pad == 0:
             padding = (0, 0, pad_split, pad_amount - pad_split)
