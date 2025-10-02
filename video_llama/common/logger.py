8782d60a765ee19cdbed2a7ff80044daf8cb6838
Chi Ho Fu <chfuab@gmail.com>
2025-10-02 20:07:08 +0800

    logger

--- video_llama/common/logger.py
+++ video_llama/common/logger.py
@@ -137,7 +137,8 @@ class MetricLogger(object):
                 loss_str.append("{}: {:.4f}".format(name, meter.global_avg()))
             elif name == "accuracy":
                 output_string.append(meter.str_record)
-        return self.delimiter.join(loss_str.extend(output_string))
+        final_list = loss_str.extend(output_string)
+        return self.delimiter.join(final_list)
 
     def synchronize_between_processes(self):
         for meter in self.meters.values():
