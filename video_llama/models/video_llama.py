bdaa72f435619a30a2e500e0b5dc96d378676c39
Chi Ho Fu <chfuab@gmail.com>
2025-10-05 17:23:01 +0800

    logger

--- video_llama/models/video_llama.py
+++ video_llama/models/video_llama.py
@@ -708,6 +708,7 @@ class VideoLLAMA(Blip2Base):
                 ans_ids == self.llama_tokenizer.pad_token_id, -100
             )
             print(f"\nempty: {empty.size()}\nreal_target: {real_target.size()}\n")
+            print(f"\nfull_embeds: {full_embeds.size()}\n")
             targets = torch.cat([empty, real_target], dim=1)
 
             with self.maybe_autocast():
