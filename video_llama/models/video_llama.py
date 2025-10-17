c0834755f4ea00cdf3a89a47cf5684aa0e1e504b
Chi Ho Fu <chfuab@gmail.com>
2025-10-17 13:23:52 +0800

    dataloader

--- video_llama/models/video_llama.py
+++ video_llama/models/video_llama.py
@@ -719,7 +719,10 @@ class VideoLLAMA(Blip2Base):
             loss = outputs.loss
             logits = outputs.logits    
             inference_embs = torch.cat([bos_embeds, sys_embeds, 
-                                     img_embeds, b_embeds], dim=1)   
+                                     a_embeds, vid_embeds_e1, text_e1_embeds, 
+                                     a_embeds, vid_embeds_e2, text_e2_embeds, 
+                                     a_embeds, vid_embeds_e3, text_e3_embeds,
+                                     a_embeds, img_embeds, b_embeds], dim=1)   
             
             return {"loss": loss, "logits": logits, "inference_embeds": inference_embs}
         
