687d28eb388bf584238ba424b1058a4a7a845037
Chi Ho Fu <chfuab@gmail.com>
2025-10-18 19:11:35 +0800

    prompt format

--- video_llama/datasets/datasets/nextQA_dataset.py
+++ video_llama/datasets/datasets/nextQA_dataset.py
@@ -107,8 +107,8 @@ class NextQATrainDataset(BaseDataset):
 
             sys_prompt = f'''<s>[INST]<<SYS>>{sys_prompt}<</SYS>>'''
             question_prompt_a = f'''Example: \nvideo_embeddings: '''
-            question_prompt_a_real = f'''real_video_embeddings: '''
-            question_prompt_b = f'''{question_prompt}[/INST]'''
+            question_prompt_a_real = f'''\nreal_video_embeddings: '''
+            question_prompt_b = f'''\n{question_prompt}[/INST]'''
 
             # fetch video
             video_path = self._get_video_path(index, is_example=False) 
