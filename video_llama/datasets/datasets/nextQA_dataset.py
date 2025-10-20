4af0264b6bd638ceabd56c26d8f1256c384ee0e9
Chi Ho Fu <chfuab@gmail.com>
2025-10-20 18:09:49 +0800

    output

--- video_llama/datasets/datasets/nextQA_dataset.py
+++ video_llama/datasets/datasets/nextQA_dataset.py
@@ -102,7 +102,7 @@ class NextQATrainDataset(BaseDataset):
             
             # sys_prompt = '''You are given video_embeddings, a question, and five options of answers to the question indexed by A, B, C, D, E. Your task is to select the correct answer to the question from the five options according to the video_embeddings.'''
             
-            sys_prompt = '''You are given video embeddings, a question. Your task is to answer the question according to the video embeddings.'''
+            sys_prompt = '''You are given video embeddings, a question, and five options of answers to the question which are indexed by A, B, C, D, E. Your task is to select the correct answer index from the five options of answers to the question according to the video embeddings.'''
             question_prompt, correct_answer = self._vqa_question(index, is_example=False)
 
             sys_prompt = f'''<s>[INST]<<SYS>>{sys_prompt}<</SYS>>'''
@@ -175,7 +175,7 @@ class NextQATrainDataset(BaseDataset):
         else:
             correct_answer_in_example = ""
         
-        question_prompt = f'''{question}'''
+        question_prompt = f'''qusetion: {question} options of answers: A.{answer_choices[0]} B.{answer_choices[1]} C.{answer_choices[2]} D.{answer_choices[3]} E.{answer_choices[4]} correct answer: {correct_answer_in_example}'''
         if is_example:
             return question_prompt
         else:
