9413a1ee11b61517d54c8ebc4ee604135fbe45b5
Chi Ho Fu <chfuab@gmail.com>
2025-10-20 14:23:38 +0800

    output

--- video_llama/datasets/datasets/nextQA_dataset.py
+++ video_llama/datasets/datasets/nextQA_dataset.py
@@ -105,7 +105,7 @@ class NextQATrainDataset(BaseDataset):
             sys_prompt = '''You are given video embeddings, a question called real_question, and five options of answers to real_question called real_answers which are indexed by A, B, C, D, E. Your task is to select the correct answer from the real_answers to the real_question according to the video embeddings. Give your answers in terms of the answer index only, that is, A, B, C, D or E.'''
             question_prompt, correct_answer = self._vqa_question(index, is_example=False)
 
-            sys_prompt = f'''<s><<SYS>>{sys_prompt}<</SYS>>'''
+            sys_prompt = f'''<<SYS>>{sys_prompt}<</SYS>>'''
             question_prompt_a = f'''Example: \nvideo_embeddings: '''
             question_prompt_a_real = f'''<Video>'''
             question_prompt_b = f'''</Video>[INST]{question_prompt}[/INST]'''
