b95a6f4ae314035034dcf9ca0aa15ec7f577640a
Chi Ho Fu <chfuab@gmail.com>
2025-10-20 14:45:20 +0800

    output

--- video_llama/datasets/datasets/nextQA_dataset.py
+++ video_llama/datasets/datasets/nextQA_dataset.py
@@ -175,7 +175,7 @@ class NextQATrainDataset(BaseDataset):
         else:
             correct_answer_in_example = ""
         
-        question_prompt = f'''qusetion: {question} \noptions of answers: \nA.{answer_choices[0]} \nB.{answer_choices[1]} \nC.{answer_choices[2]} \nD.{answer_choices[3]} \nE.{answer_choices[4]} \ncorrect answer: {correct_answer_in_example}\n'''
+        question_prompt = f'''qusetion: {question} options of answers: A.{answer_choices[0]} B.{answer_choices[1]} C.{answer_choices[2]} D.{answer_choices[3]} E.{answer_choices[4]} correct answer: {correct_answer_in_example}'''
         if is_example:
             return question_prompt
         else:
