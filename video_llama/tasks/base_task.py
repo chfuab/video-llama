a5cabb22164e3b1a5db79da542bb814404b1b1a1
Chi Ho Fu <chfuab@gmail.com>
2025-10-28 18:46:47 +0800

    output - loss

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -230,7 +230,7 @@ class BaseTask:
                 eval_output_temp = self.valid_step(model=model, samples=samples, metrics_name=name, model_name=model_name) # load the best checkpoint of the model?
                 eval_output.update(eval_output_temp)
 
-            metric_logger.update(accuracy=eval_output['accuracy'], loss=eval_output['loss'].item())
+            metric_logger.update(accuracy=eval_output['accuracy'], loss=eval_output['loss'].float())
         print(f"\nstr(metric_logger.global_avg()): {str(metric_logger.global_avg())}\n")
         logging_str = "Averaged stats: \n" + str(metric_logger.global_avg())    # getting avg over all batch size of samples in one epoch
         logging.info(logging_str)
