05e1a5599a9c5ca5d7ff278df9dd8b2c5296256b
Chi Ho Fu <chfuab@gmail.com>
2025-10-29 03:11:30 +0800

    optimizing and quantifying the textual response from llama model
    optimizing and quantifying the textual response from llama model
    
    optimizing and quantifying the textual response from llama model
    
    optimizing and quantifying the textual response from llama model

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -108,8 +108,9 @@ class BaseTask:
                     )                    
                     output_text = model.llama_tokenizer.decode(output[0], add_special_tokens=False)
                     output_final.append(output_text)
-                    final_answer = re.findall(r'[A-Z]+', output_text)[-1]
+                    final_answer = re.findall(r'\([(A-Z)]+\)', output_text)[-1].split(" ")[-1]
                     answer_label = samples["correct_ans_A-E"][i]
+                    print(f"final_answer: {final_answer}, answer_label: {answer_label}")
 
                     if final_answer == answer_label:
                         ans_scores += 1
