17d11093fc6d83a989b2aaf5ff0be985a666c63d
Chi Ho Fu <chfuab@gmail.com>
2025-10-29 03:59:07 +0800

    output - loss

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -108,7 +108,7 @@ class BaseTask:
                     )                    
                     output_text = model.llama_tokenizer.decode(output[0], add_special_tokens=False)
                     output_final.append(output_text)
-                    final_answer = re.findall(r'(.[(A-Z)]+)', output_text)[-1].split(" ")[-1]
+                    final_answer = re.findall(r'\([(A-Z)]+\)', output_text)[-1].split(" ")[-1]
                     answer_label = samples["correct_ans_A-E"][i]
                     print(f"final_answer: {final_answer}, answer_label: {answer_label}")
 
