03d7020971fcdefc7b1d468fa530a394205f1e5d
Chi Ho Fu <chfuab@gmail.com>
2025-10-28 19:42:52 +0800

    output - loss

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -230,17 +230,12 @@ class BaseTask:
                 eval_output.update(eval_output_temp)
 
             metric_logger.update(accuracy=eval_output['accuracy'], loss=eval_output['loss'])
-        logging_str = "Averaged stats: \n" + str(metric_logger.global_avg())    # getting avg over all batch size of samples in one epoch
+        logging_str = "Averaged stats: \n" + str({k: meter.global_avg() for k, meter in metric_logger.meters.items()})    # getting avg over all batch size of samples in one epoch
         logging.info(logging_str)
 
         if is_dist_avail_and_initialized():
             dist.barrier()
 
-        final_result = {
-            k: meter.global_avg()
-            for k, meter in metric_logger.meters.items()
-        }
-        print(f"\nfinal_result: {final_result}\n")
         return {
             k: meter.global_avg()
             for k, meter in metric_logger.meters.items()
