e852a8b344077a35052f898e555837cc4c76ba4f
Chi Ho Fu <chfuab@gmail.com>
2025-11-07 11:42:29 +0800

    trainning script

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -206,8 +206,7 @@ class BaseTask:
                 k: meter.value_record
                 for k, meter in metric_logger.meters.items()
             } """
-        does_dataloader_has_next = hasattr(data_loader, "__next__")
-        print(f"\nis dataloader in eval has __next__?: {does_dataloader_has_next}\n")
+        data_loader = iter(data_loader)
         if not hasattr(data_loader, "__next__"):
             # convert to iterator if not already
             data_loader = iter(data_loader)
@@ -345,8 +344,7 @@ class BaseTask:
         training stops after #iters_per_epoch iterations.
         """
         use_amp = scaler is not None
-        does_dataloader_has_next = hasattr(data_loader, "__next__")
-        print(f"\nis dataloader in train has __next__?: {does_dataloader_has_next}\n")
+
         if not hasattr(data_loader, "__next__"):
             # convert to iterator if not already
             data_loader = iter(data_loader)
