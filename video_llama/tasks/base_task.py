894d8a8e7ef780e3316660bd373d2287fadb35ac
Chi Ho Fu <chfuab@gmail.com>
2025-11-08 15:37:44 +0800

    trainning script

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -391,9 +391,11 @@ class BaseTask:
             lr_scheduler.step(cur_epoch=inner_epoch, cur_step=i)
 
             ###
-            """ initial_params = {}
-            for name, param in model.named_parameters():
-                initial_params[name] = param.data.clone() """
+            initial_params = {}
+            for name, param in model.video_Qformer.bert.encoder.layer.named_parameters():
+                initial_params[name] = param.data.clone()
+            for name, param in model.Qformer.bert.encoder.layer.named_parameters():
+                initial_params[name] = param.data.clone()
             ###
 
 
@@ -416,11 +418,16 @@ class BaseTask:
                 optimizer.zero_grad()
 
             ###
-            """ for name, param in model.named_parameters():
+            for name, param in model.video_Qformer.bert.encoder.layer.named_parameters():
                 if not torch.equal(initial_params[name], param.data):
                     print(f"Parameter '{name}' has been updated.")
                 else:
-                    print(f"Parameter '{name}' has not been updated.") """
+                    print(f"Parameter '{name}' has not been updated.")
+            for name, param in model.Qformer.bert.encoder.layer.named_parameters():
+                if not torch.equal(initial_params[name], param.data):
+                    print(f"Parameter '{name}' has been updated.")
+                else:
+                    print(f"Parameter '{name}' has not been updated.")
             ###
 
             metric_logger.update(loss=loss.item())
