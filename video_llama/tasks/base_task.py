8dc7a641bfc1c69f8a6175590e58805721f53427
Chi Ho Fu <chfuab@gmail.com>
2025-11-08 16:49:30 +0800

    training script

--- video_llama/tasks/base_task.py
+++ video_llama/tasks/base_task.py
@@ -437,7 +437,10 @@ class BaseTask:
         # gather the stats from all processes
         
         metric_logger.synchronize_between_processes()
-        logging.info("Averaged stats: " + str(metric_logger.global_avg()))
+        logging.info("Averaged stats: " + str({
+            k: "{:.6f}".format(meter.global_avg())
+            for k, meter in metric_logger.meters.items()
+        }))
         return {
             k: "{:.3f}".format(meter.global_avg())
             for k, meter in metric_logger.meters.items()
