b6cae549fa340f3a8a4ad602ebb77b91a8c4a7ba
Chi Ho Fu <chfuab@gmail.com>
2025-11-27 11:57:05 +0800

    upload video and anno_data

--- video_llama/datasets/datasets/nextQA_dataset.py
+++ video_llama/datasets/datasets/nextQA_dataset.py
@@ -53,7 +53,7 @@ class NextQATrainDataset(BaseDataset):
 
         df = pd.read_csv(os.path.join(ann_root, "anno.csv"))
         df_example = pd.read_csv(os.path.join(ann_root, "text_data_examples.csv"))
-        video_src_folder = os.path.join(vis_root, "nextqa-video")
+        video_src_folder = os.path.join(vis_root, "nextqa-video/NExTVideo")
         video_des_folder = os.path.join(vis_root, "nextqa-video-extracted")
 
         self.video_src_folder = video_src_folder
