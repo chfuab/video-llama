6ee982c0cfd26d8a28912c592c6b837ab8216e43
Chi Ho Fu <chfuab@gmail.com>
2025-12-05 19:58:28 +0800

    reload checkpoint for evaluation

--- video_llama/runners/runner_base.py
+++ video_llama/runners/runner_base.py
@@ -655,7 +655,7 @@ class RunnerBase:
         # TODO consider moving to model.before_evaluation()
         model = self.unwrap_dist_model(self.model)
         if not skip_reload and cur_epoch == "best":
-            model = self._reload_best_model(model, 7)
+            model = self._reload_best_model(model, cur_epoch)
         elif not skip_reload and cur_epoch != "best" and (cur_epoch % 5 == 0) and (cur_epoch // 5 > 0):
             model = self._reload_best_model(model, cur_epoch - 1)
         model.eval()
