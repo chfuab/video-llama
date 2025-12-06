a7d7b6aca8fb3595e8c47d4291bad2caf65893be
Chi Ho Fu <chfuab@gmail.com>
2025-12-06 19:52:11 +0800

    checking evaluation loss using checkpoint_best

--- video_llama/runners/runner_base.py
+++ video_llama/runners/runner_base.py
@@ -665,7 +665,7 @@ class RunnerBase:
         if not skip_reload and cur_epoch == "best":
             model = self._reload_best_model(model, cur_epoch)
         elif not skip_reload and cur_epoch != "best" and (cur_epoch % 5 == 0) and (cur_epoch // 5 > 0):
-            model = self._reload_best_model(model, cur_epoch - 1)
+            model = self._reload_best_model(model, cur_epoch)
         model.eval()
 
         ###
